af2359482e4c1b62df9fed7266b2b8685587eaa7
Igor Vila <103008816+igorvila01@users.noreply.github.com>
2025-12-30 15:23:53 +0000

    melhorando a funcao de gerador de numeros para somente uma função

--- menu.py
+++ menu.py
@@ -1,20 +1,26 @@
 import random
 
-def gerador_numeros_mega(qtd): 
-    qtd = len(jogos_ms)+qtd   
-    while len(jogos_ms) < qtd:
-        aposta = sorted(random.sample(range(1,61),6)) 
-        if aposta not in jogos_ms.values():
-            jogo = f'{len(jogos_ms)+1}'
-            jogos_ms[jogo] = aposta
+def gerador_numeros(opcao, qtd):
+    match opcao:
+        # Gerador Mega Sena
+        case 1:
+            qtd = len(jogos_ms)+qtd   
+            while len(jogos_ms) < qtd:
+                aposta = sorted(random.sample(range(1,61),6)) 
 
-def gerador_numeros_lotofacil(qtd):
-    qtd = len(jogos_lf)+qtd
-    while len(jogos_lf) < qtd:
-        aposta = sorted(random.sample(range(1,26),15))
-        jogo = f'{len(jogos_lf)+1}'
-        jogos_lf[jogo] = aposta
-
+                if aposta not in jogos_ms.values():
+                    jogo = f'{len(jogos_ms)+1}'
+                    jogos_ms[jogo] = aposta
+            print('Jogos Mega Sena Realizado com sucesso:')
+        
+        case 2:
+            qtd = len(jogos_lf)+qtd
+            while len(jogos_lf) < qtd:
+                aposta = sorted(random.sample(range(1,26),15))
+                
+                if aposta not in jogos_lf.values():
+                    jogo = f'{len(jogos_lf)+1}'
+                    jogos_lf[jogo] = aposta
 
 
 def lista_jogos(jogos):
@@ -54,11 +60,21 @@ while True:
                     print('Quantidade Invalida! Digite novamente!')
                     continue            
             
-            gerador_numeros_mega(qtd_jogos)
+            gerador_numeros(menu_selecionado, qtd_jogos)
             lista_jogos(jogos_ms)
         
         case 2:
-            ...
+            while True:
+                try:
+                    qtd_jogos = int(input('Digite a quantidade de jogos que quer gerar:'))
+                    print()
+                    break
+                except ValueError:
+                    print('Entrada Invalida! tente novamente!')
+                    continue
+            
+            gerador_numeros(menu_selecionado, qtd_jogos)
+            lista_jogos(jogos_lf)
         
         case 3:
             ...
